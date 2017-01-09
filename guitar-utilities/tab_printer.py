1bfe23a0d1d71dc018497b40e4c15c4f1dda2f7b
wilsonchaney <wilsongchaney@gmail.com>
2017-01-09 10:27:29 -1000

    add tab printing utility

---
+++ guitar-utilities/tab_printer.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+import logging
+
+Fingering = namedtuple("Fingering", "string fret")
+
+# Logger used for outputting tabs - avoids ugliness/interleaved lines when using print
+log = logging.getLogger("Tab")
+
+def print_melody_tab(melody, durations=None, fname=None):
+    """
+    Prints a guitar tab, either to stdout or a file. It only works with single note melodies for now.
+
+    Args:
+        melody: list of Fingering structs.
+        durations: list of floats, representing the duration of each note. It can be in beats, seconds, whatever. It will be scaled so the tab looks decent.
+        fname: If included, method will write tab to file. Otherwise, tab goes to stdout.
+    """
+
+    if durations and len(durations) != len(melody):
+        raise ValueError("Length of durations must match length of melody: %d != %d" % (len(durations), len(melody)))
+
+    # scale durations to a reasonable integer for printing
+    if durations:
+        durations = map(lambda d: int(d * 64), durations)
+        min_dur = min(durations)
+        durations = map(lambda d: int(d / min_dur), durations)
+
+    n = len(melody)
+
+    lines = ["|--"] * 6
+
+    # 2 characters per note, and 2 characters of spacing (unless durations is defined)
+    for i in range(n):
+        f = melody[i]
+        for j in range(6):
+            if f.string == j:
+                # Write the fret number
+                if f.fret >= 10:
+                    lines[j] += str(f.fret)
+                else:
+                    lines[j] += str(f.fret) + "-"
+            else:
+                lines[j] += "--"
+        # Add spacing
+        if durations:
+            lines = map(lambda line: line + "-" * durations[i], lines)
+        else:
+            # Default (if durations aren't provided) is to just add two hyphens.
+            lines = map(lambda line: line + "--", lines)
+
+    # Finish each line cleanly
+    for i in range(5, -1, -1):
+        lines[i] += "----|\n"
+
+    # Output in reverse order (because string order is high to low in printed tabs.
+    if fname:
+        with open(fname, "w") as outfile:
+            for line in reversed(lines):
+                outfile.write(line)
+    else:
+        # One print call, because lines can be grossly interspersed if debug logs aren't flushed.
+        output = '\n' + ''.join(reversed(lines))
+        log.info(output)
+
